eaf01e181dbf09534b30023e9633141ae21928aa
tkathuria2002 <tarun91@vt.edu>
2018-09-24 22:00:43 -0400

    Adding new project files for DE1 with automatic jic creation

---
+++ designs/leon3-terasic-de1/generate_jic.tcl
@@ -0,0 +1,9 @@
+set module [lindex $quartus(args) 0] 
+if [string match "quartus_map" $module] {# Generating .jic file based on .cof file 
+set cmd "quartus_cpf -c convertsoftojic.cof" 
+post_message "Generated .jic file with date and time" 
+# If the command can't be run, return an error. 
+if { [catch {open "|$cmd"} input] } { 
+return -code error $input 
+} 
+}
