2e498868798274426d7523808978939e7c1a2303
tkathuria2002 <tarun91@vt.edu>
2018-10-11 14:51:09 -0400

    Adding timerlap to gptimer API

--- software/API/src/gptimer.c
+++ software/API/src/gptimer.c
@@ -1,5 +1,6 @@
 #include "gptimer.h"
 gptimer *lr = (gptimer *) ADDR;
+unsigned long int count=MAX_VALUE;
 void timer_start()
 {
         int ntimers;
@@ -23,4 +24,22 @@ unsigned long int timer_stop()
 	lr->timer[0].control = 0x4;     //Reset timer0 to original value
 	return exec_tics;
 
+}
+
+unsigned long int timerlap()
+{
+	unsigned long int lap;
+	if(count==MAX_VALUE)
+	{
+		lr->scalerload = RESOLUTION;   //Load prescaler with clock division factor
+		lr->scalercnt = RESOLUTION;    //This division factor is also the reolution
+		lr->timer[0].control = 0;
+		lr->timer[0].counter = MAX_VALUE; //Load counter0 with largest 32 bit value
+		lr->timer[0].reload = MAX_VALUE;  //Also set the reload value to largest 32 bit value
+		lr->timer[0].control = 0xf;	  //Enable timer0 
+	}
+	
+	lap = (unsigned long int) (count - lr->timer[0].counter) * RESOLUTION;
+	count = lr->timer[0].counter;
+	return lap;
 }
