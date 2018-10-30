e3b2c727a660a07a2134157b1c4d09ccfa1656d4
tkathuria2002 <tarun91@vt.edu>
2018-10-30 12:11:08 -0400

    Fixing redclaration of ptr in keymill API

--- software/API/src/keymill.c
+++ software/API/src/keymill.c
@@ -28,46 +28,46 @@ void main()
 
 
 #include "keymill.h"
-keymill_reg *ptr = (keymill_reg *)KEYMILL_BASE_ADDR;
+keymill_reg *keymill_ptr = (keymill_reg *)KEYMILL_BASE_ADDR;
 
 void keymill_input_key(char key[16]) //Function to take input key
 {
-		ptr->input_key0 = *((unsigned int *)key);
-		ptr->input_key1 = *((unsigned int *)(key+4));
-		ptr->input_key2 = *((unsigned int *)(key+8));
-		ptr->input_key3 = *((unsigned int *)(key+12));
-		printf("\nKey values are %x %x %x %x\n", ptr->input_key0, ptr->input_key1, ptr->input_key2, ptr->input_key3);
+		keymill_ptr->input_key0 = *((unsigned int *)key);
+		keymill_ptr->input_key1 = *((unsigned int *)(key+4));
+		keymill_ptr->input_key2 = *((unsigned int *)(key+8));
+		keymill_ptr->input_key3 = *((unsigned int *)(key+12));
+		printf("\nKey values are %x %x %x %x\n", keymill_ptr->input_key0, keymill_ptr->input_key1, keymill_ptr->input_key2, keymill_ptr->input_key3);
 }
 
 void keymill_input_iv(char iv[16]) //Function to take input key
 {
-		ptr->input_iv0 = *((unsigned int *)iv);
-		ptr->input_iv1 = *((unsigned int *)(iv+4));
-		ptr->input_iv2 = *((unsigned int *)(iv+8));
-		ptr->input_iv3 = *((unsigned int *)(iv+12));
-		printf("\nInit_vector values are %x %x %x %x\n", ptr->input_iv0, ptr->input_iv1, ptr->input_iv2, ptr->input_iv3);
+		keymill_ptr->input_iv0 = *((unsigned int *)iv);
+		keymill_ptr->input_iv1 = *((unsigned int *)(iv+4));
+		keymill_ptr->input_iv2 = *((unsigned int *)(iv+8));
+		keymill_ptr->input_iv3 = *((unsigned int *)(iv+12));
+		printf("\nInit_vector values are %x %x %x %x\n", keymill_ptr->input_iv0, keymill_ptr->input_iv1, keymill_ptr->input_iv2, keymill_ptr->input_iv3);
 }
 
 void start_keymill() //Function to start the key generation
 {
-	ptr->keymill_control = 0x00000001;
+	keymill_ptr->keymill_control = 0x00000001;
 }
 
 void reset_keymill() //Software reset keymill
 {
-	ptr->keymill_control = 0x00000002;
+	keymill_ptr->keymill_control = 0x00000002;
 }
 
 unsigned int keymill_output() //Get 32b output from keymill
 {	
 	unsigned int output;
 	int i;
-	while((ptr->keymill_control & 0x80000000) != 0);
+	while((keymill_ptr->keymill_control & 0x80000000) != 0);
 	
 	for(i=0;i<4; i++)
 	{ 
-		while((ptr->keymill_control & 0x40000000) != 0);
-		output = ptr->output;
+		while((keymill_ptr->keymill_control & 0x40000000) != 0);
+		output = keymill_ptr->output;
 	}
 	return output;
 }
