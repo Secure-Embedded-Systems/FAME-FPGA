edf4d6aa9e97897d51ec555c8e2c6b9160b43405
tkathuria2002 <tarun91@vt.edu>
2018-11-12 19:07:16 -0500

    Cleaning up UART API

--- software/API/src/uart.c
+++ software/API/src/uart.c
@@ -4,36 +4,32 @@
 #define SYS_CLK 50
 #define UART_BASE_ADDR 0x80000100
 
-typedef struct {
-	volatile unsigned int data;
-	volatile unsigned int status;
-	volatile unsigned int control;
-	volatile unsigned int scaler;
-	volatile unsigned int fifo_debug;
-}uart_reg;
+typedef enum {
+DATA,
+STATUS,
+CONTROL,
+SCALER,
+FIFO_DEBUG } UARTREGID_T;
 
+#define UARTREG (((volatile unsigned *) UART_BASE_ADDR))
 void init_uart() {
-  uart_reg *ptr = (uart_reg *)UART_BASE_ADDR;
-  ptr->data = 0; //Clear the data register
-  ptr->scaler = (SYS_CLK*1000000)/(BAUDRATE*8+7);//Set scaler value according to system clock and baudrate
-  ptr->control = 0x3;	//Enable transmitter and receiver
+  UARTREG[DATA] = 0; //Clear the data register
+  UARTREG[SCALER] = (SYS_CLK*1000000)/(BAUDRATE*8+7);//Set scaler value according to system clock and baudrate
+  UARTREG[CONTROL] = 0x3;	//Enable transmitter and receiver
 }
 
 void set_baudrate_uart(int baudrate) {
-  uart_reg *ptr = (uart_reg *)UART_BASE_ADDR;
-  ptr->scaler = (SYS_CLK*1000000)/(baudrate*8+7);  
+  UARTREG[SCALER] = (SYS_CLK*1000000)/(baudrate*8+7);  
 }
 
 void put_char_uart(char a) {
-  uart_reg *ptr = (uart_reg *)UART_BASE_ADDR;
-  while(ptr->status & (1<<9)); //Wait if transmission fifo is full
-  ptr->data = a;
+  while(UARTREG[STATUS] & (1<<9)); //Wait if transmission fifo is full
+  UARTREG[DATA] = a;
 }
 
 char get_char_uart() {
-  uart_reg *ptr = (uart_reg *)UART_BASE_ADDR;
-  while(!(ptr->status & 0x1)); //Wait till data ready
-  return ptr->data; 
+  while(!(UARTREG[STATUS] & 0x1)); //Wait till data ready
+  return UARTREG[DATA]; 
   
 }
 
@@ -57,15 +53,14 @@ void main()
 	volatile long long cnt;
 	init_uart();
 	for(cnt=0;cnt<100;cnt++)
-		put_char('A');
-	printf("input_char: %c",get_char());
+		put_char_uart('A');
+	printf("input_char: %c",get_char_uart());
 	char test[] = "Tarun";
-	put_string(test,5);
+	put_string_uart(test,5);
 	char* output = (char*)malloc(5);
-	get_string(output,5);
+	get_string_uart(output,5);
 	for(cnt=0; cnt<5;cnt++)
 	printf("input_string: %c",output[cnt]);
 	free(output);
 }	
 */
-
