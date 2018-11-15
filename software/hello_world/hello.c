ea44137b616b186a2b67699e8803e5469223a2df
Patrick Schaumont <schaum@vt.edu>
2018-11-14 21:39:31 -0500

    hello

--- software/hello_world/hello.c
+++ software/hello_world/hello.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include "uart.h"
+
+//int __bcc_con_outbyte(char c) {
+//  put_char_uart(c);
+//}
+
 void main() {
-	unsigned i;
-	for (i=0; i<20; i++)
-	  printf("Hello World %d\n", i);
+  unsigned i;
+  init_uart();
+  for (i=0; i<20; i++) {
+    printf("Hello World %d\n", i);
+  }
 }
